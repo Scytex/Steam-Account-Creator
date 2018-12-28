d9fa21fb6eb1cf6cd53a8cddd7e04b186bb51932
SpookedUrMumHaker <40287619+urmumusesmyhakerino@users.noreply.github.com>
2018-12-28 12:23:27 -0500

    Fixed crash when writing to files, and added ability to choose email, and formatting style.

--- SteamAccCreator/File/FileManager.cs
+++ SteamAccCreator/File/FileManager.cs
@@ -1,22 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.IO;
+using System.Threading.Tasks;
+using SteamAccCreator.Gui;
 
 namespace SteamAccCreator.File
 {
     public class FileManager
     {
-        private const string Path = @"accounts.txt";
-
-        public void WriteIntoFile(string mail, string alias, string pass)
+        public void WriteIntoFile(string mail, bool writeMail, string alias, string pass, string path, string style)
         {
-            using (var writer = new StreamWriter(Path, true))
+
+            Task.Delay(2000).Wait();
+
+            using (var writer = new StreamWriter(path, true))
             {
-                writer.WriteLine("Mail: \t\t" + mail);
-                writer.WriteLine("Alias: \t\t" + alias);
-                writer.WriteLine("Pass: \t\t" + pass);
-                writer.WriteLine("Creation: \t" + DateTime.Now);
-                writer.WriteLine("###########################");
+                if (style == "User:Pass Formatting")
+                {
+                    if (writeMail == false)
+                    {
+                        writer.WriteLine(alias + ":" + pass);
+                    }
+                    else
+                    {
+                        writer.WriteLine(alias + ":" + pass + ":" + mail);
+                    }
+                } else if (style == "Original Formatting")
+                {
+                    if (writeMail == false)
+                    {
+                        writer.WriteLine("Alias: \t\t" + alias);
+                        writer.WriteLine("Pass: \t\t" + pass);
+                        writer.WriteLine("Creation: \t" + DateTime.Now);
+                        writer.WriteLine("###########################");
+                    }
+                    else
+                    {
+                        writer.WriteLine("Mail: \t\t" + mail);
+                        writer.WriteLine("Alias: \t\t" + alias);
+                        writer.WriteLine("Pass: \t\t" + pass);
+                        writer.WriteLine("Creation: \t" + DateTime.Now);
+                        writer.WriteLine("###########################");
+                    }
+                }
             }
         }
     }
